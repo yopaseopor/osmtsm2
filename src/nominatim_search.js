f59a89498062f09a3d0ef7e4a4ed6bc9bda7caf4
yopaseopor <yopaseopor@gmail.com>
2025-04-12 01:32:15 +0200

    new searcher

--- src/nominatim_search.js
+++ src/nominatim_search.js
@@ -8,13 +8,16 @@ function initNominatimSearch(map) {
             .html($('<button type="button"><i class="fa fa-search"></i></button>'));
         
         var searchInput = $('<input type="text" placeholder="Search...">').hide();
-        container.append(searchInput);
+        var resultsList = $('<ul>').addClass('search-results').hide();
+        container.append(searchInput).append(resultsList);
 
         container.on('click', 'button', function(e) {
             e.preventDefault();
             searchInput.toggle();
             if (searchInput.is(':visible')) {
                 searchInput.focus();
+            } else {
+                resultsList.hide();
             }
         });
 
@@ -31,51 +34,83 @@ function initNominatimSearch(map) {
                         data: {
                             q: query,
                             format: 'json',
-                            limit: 1
+                            limit: 10,
+                            addressdetails: 1
                         },
                         success: function(data) {
+                            resultsList.empty();
                             if (data && data.length > 0) {
-                                var result = data[0];
-                                var coordinates = ol.proj.fromLonLat([
-                                    parseFloat(result.lon),
-                                    parseFloat(result.lat)
-                                ]);
-
-                                // Animate to location
-                                map.getView().animate({
-                                    center: coordinates,
-                                    zoom: 17,
-                                    duration: 1000
+                                data.forEach(function(result) {
+                                    var displayName = result.display_name.split(',')[0] + 
+                                                    (result.address ? ', ' + (result.address.city || result.address.town || result.address.village || '') : '');
+                                    
+                                    $('<li>')
+                                        .text(displayName)
+                                        .data('result', result)
+                                        .appendTo(resultsList);
                                 });
-
-                                // Add temporary marker
-                                var markerElement = $('<div>')
-                                    .addClass('nominatim-marker')
-                                    .html('<i class="fa fa-map-marker"></i>')[0];
-
-                                var marker = new ol.Overlay({
-                                    position: coordinates,
-                                    positioning: 'bottom-center',
-                                    element: markerElement,
-                                    offset: [0, -20]
-                                });
-                                map.addOverlay(marker);
-
-                                // Remove marker after 5 seconds
-                                setTimeout(function() {
-                                    map.removeOverlay(marker);
-                                }, 5000);
+                                resultsList.show();
+                            } else {
+                                resultsList.append($('<li>').addClass('no-results').text('No results found'));
+                                resultsList.show();
                             }
                             container.removeClass('loading');
-                            searchInput.hide().val('');
                         },
                         error: function() {
                             container.removeClass('loading');
-                            searchInput.hide().val('');
+                            resultsList.empty()
+                                .append($('<li>').addClass('no-results').text('Search error'))
+                                .show();
                             console.error('Geocoding error');
                         }
                     });
                 }
+            }
+        });
+
+        resultsList.on('click', 'li', function() {
+            if (!$(this).hasClass('no-results')) {
+                var result = $(this).data('result');
+                var coordinates = ol.proj.fromLonLat([
+                    parseFloat(result.lon),
+                    parseFloat(result.lat)
+                ]);
+
+                // Animate to location
+                map.getView().animate({
+                    center: coordinates,
+                    zoom: 17,
+                    duration: 1000
+                });
+
+                // Add temporary marker
+                var markerElement = $('<div>')
+                    .addClass('nominatim-marker')
+                    .html('<i class="fa fa-map-marker"></i>')[0];
+
+                var marker = new ol.Overlay({
+                    position: coordinates,
+                    positioning: 'bottom-center',
+                    element: markerElement,
+                    offset: [0, -20]
+                });
+                map.addOverlay(marker);
+
+                // Remove marker after 5 seconds
+                setTimeout(function() {
+                    map.removeOverlay(marker);
+                }, 5000);
+
+                // Hide search UI
+                searchInput.hide().val('');
+                resultsList.hide();
+            }
+        });
+
+        // Close results when clicking outside
+        $(document).on('click', function(e) {
+            if (!$(e.target).closest('.nominatim-search').length) {
+                resultsList.hide();
             }
         });
 
