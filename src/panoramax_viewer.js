2c8d88799210cd1b769951b45e5482a2132cdbc1
yopaseopor <yopaseopor@gmail.com>
2025-04-12 20:05:43 +0200

    remove chevron left and chevron right

--- src/panoramax_viewer.js
+++ src/panoramax_viewer.js
@@ -77,12 +77,11 @@ function initPanoraMaxViewer(map) {
     function showPanoraMaxViewer(lat, lon, zoom) {
         // Build URL with official parameters
         var url = `https://panoramax.xyz/#` +
-            `focus=pic&` + // Show picture as main element
+            `focus=map&` + // Show map as main element
             `map=${zoom}/${lat}/${lon}&` + // Map position
             `nav=any&` + // Allow navigation between pictures
             `theme=default&` + // Use default theme
-            `background=streets&` + // Use streets background
-            `speed=1000`; // Set sequence play speed
+            `background=streets`; // Use streets background
         
         var iframe = $('#panoramax-iframe');
         
