8f815c4fb313b1dc0398f1c5342e6d1e1c906cf9
yopaseopor <yopaseopor@gmail.com>
2025-04-21 22:06:26 +0200

    test

--- overlays/Comercio.js
+++ overlays/Comercio.js
@@ -1,8 +1,8 @@
-export const Alimentacion = [
+export const Comercio = [
     {
-        group: 'Alimentación',
-        title: 'Supermercados',
-        query: '(nwr["shop"="supermarket"]({{bbox}});node(w););out meta;',
+        group: 'Comercio',
+        title: 'Mayoristas',
+        query: '(nwr["shop"="wholesale"]({{bbox}});node(w););out meta;',
         iconSrc: 'src/img/icones/maxspeed_empty.svg',
         iconStyle: 'background-color:rgba(255,255,255,0.4)',
         style: function (feature) {
@@ -10,10 +10,10 @@ export const Alimentacion = [
             var name_key = feature.getKeys().filter(function(t){return t.match(key_regex)}).pop() || "name";
             var name = feature.get(name_key) || '';
             var fill = new ol.style.Fill({
-                color: 'rgba(117,63,79,0.4)',
+                color: 'rgba(255,0,0,0.4)',
             });
             var stroke = new ol.style.Stroke({
-                color: 'rgba(117,63,79,1)',
+                color: 'rgba(255,0,0,1)',
                 width: 1,
             });
             var style = new ol.style.Style({
@@ -33,5 +33,5 @@ export const Alimentacion = [
             return style;
         },
     },
-    // Add other overlays in the "Alimentación" group here
+    // Add other overlays in the "Comercio" group here
 ];
