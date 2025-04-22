eb4c2181ce9970eeb5a7935605c2e334488030ff
yopaseopor <yopaseopor@gmail.com>
2025-04-22 23:51:16 +0200

    retest

--- src/overlays/index.js
+++ src/overlays/index.js
@@ -3,4 +3,6 @@ import { translatedOverlays } from './translated_overlays.js';
 // Export all overlays
 export const allOverlays = [
     ...translatedOverlays
-]; 
+];
+
+export default allOverlays; 
