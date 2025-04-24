22bf81b449994f31621314c3b230d90606940f7c
yopaseopor <yopaseopor@gmail.com>
2025-04-24 08:17:52 +0200

    test new overlays

---
+++ src/overlays/external/food.js
@@ -0,0 +1,40 @@
+import { getTranslation } from '../../i18n/index.js';
+
+export const foodOverlays = [
+    {
+        group: getTranslation('food'),
+        title: 'McDonald\'s',
+        query: '(nwr["brand:wikidata"="Q38076"]({{bbox}});node(w););out meta;',
+        iconSrc: 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/36/McDonald%27s_Golden_Arches.svg/220px-McDonald%27s_Golden_Arches.svg.png',
+        iconStyle: 'background-color:rgba(255,255,255,0.4)',
+        style: function (feature) {
+            var key_regex = /^name$/;
+            var name_key = feature.getKeys().filter(function(t){return t.match(key_regex)}).pop() || "name";
+            var name = feature.get(name_key) || '';
+            var fill = new ol.style.Fill({
+                color: 'rgba(255,0,0,0.4)'
+            });
+            var stroke = new ol.style.Stroke({
+                color: 'rgba(255,0,0,1)',
+                width: 1
+            });
+            var style = new ol.style.Style({
+                image: new ol.style.Icon({
+                    src: 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/36/McDonald%27s_Golden_Arches.svg/220px-McDonald%27s_Golden_Arches.svg.png',
+                    scale: 0.10
+                }),
+                text: new ol.style.Text({
+                    text: name,
+                    offsetX: 7,
+                    offsetY: -12,
+                    fill: new ol.style.Fill({
+                        color: 'rgba(0,0,0,1)'
+                    }),
+                }),
+                fill: fill,
+                stroke: stroke
+            });
+            return style;
+        }
+    }
+]; 
--- src/overlays/index.js
+++ src/overlays/index.js
@@ -1,8 +1,10 @@
 import { translatedOverlays } from './translated_overlays.js';
+import { externalOverlays } from './external/index.js';
 
 // Export all overlays
 export const allOverlays = [
-    ...translatedOverlays
+    ...translatedOverlays,
+    ...externalOverlays
 ];
 
 // Make overlays available globally for non-module scripts
