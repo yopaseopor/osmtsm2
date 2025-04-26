efa9055224e2cab8304f08c778dd44516babc80e
yopaseopor <yopaseopor@gmail.com>
2025-04-26 09:34:59 +0200

    Update loader.js

--- src/overlays/external/loader.js
+++ src/overlays/external/loader.js
@@ -1,7 +1,12 @@
 import { foodOverlays } from './food.js';
 import { customOverlays } from './custom_overlays.js';
+import { shoppingOverlays } from '../groups/shopping.js';
 
 // Function to load external overlay files
 export function loadExternalOverlays() {
-    return Promise.resolve([...foodOverlays, ...customOverlays]);
+    return Promise.resolve([
+        ...foodOverlays,
+        ...shoppingOverlays,
+        ...customOverlays
+    ]);
 } 
