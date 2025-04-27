d3adba38b35b2289951866a4e8a4cba560f3bc55
yopaseopor <yopaseopor@gmail.com>
2025-04-27 22:49:30 +0200

    Update config.js

--- src/config.js
+++ src/config.js
@@ -166,7 +166,8 @@ var config = {
         format: new ol.format.MVT(),
 		crossOrigin: 'anonymous',
 		attributions: '&copy; <a href="https://www.openstreetmap.org/" target="_blank">OpenStreetMap</a>',
-        url: 'https://vector.openstreetmap.org/shortbread_v1/{z}/{x}/{y}.mvt'
+        url: 'https://vector.openstreetmap.org/shortbread_v1/{z}/{x}/{y}.mvt',
+		attributions: '<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>'
       }),
 			visible: false,
 						styleJson: 'https://americanamap.org/style.json', // Link to the style.json
@@ -180,7 +181,7 @@ var config = {
         tileGrid: ol.tilegrid.createXYZ({maxZoom: 19}),
         format: new ol.format.MVT(),
 		crossOrigin: 'anonymous',
-		attributions: '&copy; <a href="https://www.openstreetmap.org/" target="_blank">OpenStreetMap</a>',
+		attributions: '<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>',
         url: 'https://api.maptiler.com/tiles/v3-openmaptiles/{z}/{x}/{y}.pbf?key=zPfUiHM0YgsZAlrKRPNg'
       }),
 			visible: false,
@@ -207,6 +208,7 @@ var config = {
         tileSize: 512,
         crossOrigin: 'anonymous',
         url: 'https://api.maptiler.com/tiles/v3-openmaptiles/tiles.json?key=zPfUiHM0YgsZAlrKRPNg'
+		attributions: '<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>',
       }),
 			visible: false
 		}),
