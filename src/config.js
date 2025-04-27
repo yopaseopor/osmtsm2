06b206db6e45641b7c9bf2b912cdefb61e343583
yopaseopor <yopaseopor@gmail.com>
2025-04-27 20:14:57 +0200

    test

--- src/config.js
+++ src/config.js
@@ -6,6 +6,7 @@
 
 //@@ Ruta de imágenes
 var imgSrc = 'src/img/';
+import { vectorTileStyle } from './vector_tile_style.js';
 
 //@@Coordenadas LONgitud LATitud Rotación Zoom, Zoom de la geolocalización, unidades
 var config = {
@@ -43,6 +44,21 @@ var config = {
 	},
 	//@@ Mapas de fondo
 	layers: [
+		// Shortbread Vector Tile Layer
+		new ol.layer.VectorTile({
+			title: 'Shortbread Vector',
+			iconSrc: imgSrc + 'icones_web/osm_logo-layer.svg',
+			style: vectorTileStyle, // Imported style function
+			source: new ol.source.VectorTile({
+				tilePixelRatio: 1,
+				tileGrid: ol.tilegrid.createXYZ({ maxZoom: 14 }),
+				format: new ol.format.MVT(),
+				crossOrigin: 'anonymous',
+				attributions: '&copy; <a href="https://www.openstreetmap.org/" target="_blank">OpenStreetMap</a>',
+				url: 'https://vector.openstreetmap.org/shortbread_v1/{z}/{x}/{y}.mvt'
+			}),
+			visible: false
+		}),
 		new ol.layer.Tile({
 			title: 'OpenStreetMap',
 			iconSrc: imgSrc + 'icones_web/osm_logo-layer.svg',
@@ -195,20 +211,6 @@ var config = {
 			visible: false
 		}),
 		
-		        new ol.layer.VectorTile({ // Shortbread Vector Tiles https://shortbread-tiles.org/schema/1.0/
-            title: 'Shortbread Vector',
-            iconSrc: imgSrc + 'icones_web/shortbread_logo_layer.png', // Placeholder icon, add your own if available
-            source: new ol.source.VectorTile({
-                tilePixelRatio: 1,
-                tileGrid: ol.tilegrid.createXYZ({ maxZoom: 19 }),
-                format: new ol.format.MVT(),
-                attributions: '&copy; <a href="https://www.openstreetmap.org/" target="_blank">OpenStreetMap</a> contributors | <a href="https://shortbread-tiles.org/schema/1.0/" target="_blank">Shortbread Style</a>',
-                crossOrigin: 'anonymous',
-                url: 'https://vector.openstreetmap.org/shortbread_v1/{z}/{x}/{y}.mvt'
-            }),
-            visible: false
-        }),
-
 		new ol.layer.VectorTile({// OpenStreetMap France https://openstreetmap.fr
 			title: 'Vector OSM',
 			iconSrc: imgSrc + 'icones_web/osmfr_logo-layer.png',
