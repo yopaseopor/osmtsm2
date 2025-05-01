2e71fa644765c0ab3adb5b45c9e64b3f12aa122e
yopaseopor <yopaseopor@gmail.com>
2025-05-02 01:09:48 +0200

    Update shopping.js

--- src/overlays/groups/shopping.js
+++ src/overlays/groups/shopping.js
@@ -5,7 +5,7 @@ export const shoppingOverlays = [
         group: getTranslation('shopping'),
         title: 'Zara',
         query: '[out:json][timeout:25];(nwr["brand:wikidata"="Q147662"]({{bbox}}););out body;>;out skel qt;',
-        iconSrc: 'https://upload.wikimedia.org/wikipedia/commons/thumb/7/7d/Zara_Logo.svg/1200px-Zara_Logo.svg.png',
+        iconSrc: "https://upload.wikimedia.org/wikipedia/commons/thumb/7/7d/Zara_Logo.svg/1200px-Zara_Logo.svg.png",
         iconStyle: 'background-color:rgba(255,255,255,0.4)',
         style: function (feature) {
             var key_regex = /^name$/;
@@ -20,7 +20,7 @@ export const shoppingOverlays = [
             });
             var style = new ol.style.Style({
                 image: new ol.style.Icon({
-                    src: 'https://upload.wikimedia.org/wikipedia/commons/thumb/7/7d/Zara_Logo.svg/1200px-Zara_Logo.svg.png',
+                    src: "https://upload.wikimedia.org/wikipedia/commons/thumb/7/7d/Zara_Logo.svg/1200px-Zara_Logo.svg.png",
                     scale: 0.10
                 }),
                 text: new ol.style.Text({
