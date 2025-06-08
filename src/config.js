721d4d67a48150bd810a21bdc15bffcb2bef5d54
yopaseopor <yopaseopor@gmail.com>
2025-06-08 14:33:30 +0200

    ol 10.5

--- src/config.js
+++ src/config.js
@@ -50,24 +50,18 @@ var config = {
 			visible: false,
 			opacity: 1.0,
 			source: new ol.source.VectorTile({
-				url: function(tileCoord) {
-					const z = tileCoord[0];
-					const x = tileCoord[1];
-					const y = -tileCoord[2] - 1;
-					return `https://api.maptiler.com/tiles/v3/${z}/${x}/${y}.pbf?key=tKDOqJGURiimBRaaKrDJ`;
-				},
+				projection: 'EPSG:3857',
 				format: new ol.format.MVT(),
+				url: 'https://api.maptiler.com/tiles/v3/{z}/{x}/{y}.pbf?key=tKDOqJGURiimBRaaKrDJ',
 				tileGrid: ol.tilegrid.createXYZ({
-					extent: ol.proj.get('EPSG:3857').getExtent(),
 					minZoom: 0,
-					maxZoom: 14,
-					tileSize: 512
+					maxZoom: 14
 				}),
 				attributions: [
-					'<a href="https://www.maptiler.com/copyright/" target="_blank">MapTiler</a>',
-					'<a href="https://www.openstreetmap.org/copyright" target="_blank">OpenStreetMap contributors</a>'
-				]
-			}),
+						'<a href="https://www.maptiler.com/copyright/" target="_blank">MapTiler</a>',
+						'<a href="https://www.openstreetmap.org/copyright" target="_blank">OpenStreetMap contributors</a>'
+					]
+				}),
 				style: (function() {
 				// Initialize style configuration with glyphs and sprites
 				window.maptilerStyleConfig = {
@@ -94,32 +88,17 @@ var config = {
 					});
 				});
 
-				const styleCache = {};
+				// Return the style function with access to the config
 				return function(feature, resolution) {
-					const layer = feature.get('layer');
-					const type = feature.getGeometry().getType();
-					const key = `${layer}-${type}`;
-					
-					if (!styleCache[key]) {
-						let style;
-						if (window.vectorTileStyle) {
-							try {
-								style = window.vectorTileStyle(feature, resolution, window.maptilerStyleConfig || {});
-							} catch (e) {
-								console.error('Error in vectorTileStyle:', e);
-							}
+					if (window.vectorTileStyle) {
+						try {
+							return window.vectorTileStyle(feature, resolution, window.maptilerStyleConfig);
+						} catch (e) {
+							console.error('Error in vectorTileStyle:', e);
+							return [];
 						}
-						styleCache[key] = style || new ol.style.Style({
-							fill: new ol.style.Fill({
-								color: 'rgba(200, 200, 200, 0.4)'
-							}),
-							stroke: new ol.style.Stroke({
-								color: '#3399CC',
-								width: 1.25
-							})
-						});
 					}
-					return styleCache[key];
+					return [];
 				};
 			})()
 		}),
@@ -130,19 +109,43 @@ var config = {
 			iconSrc: imgSrc + 'icones_web/osm_logo-layer.svg',
 			visible: true,
 			source: new ol.source.VectorTile({
-				url: function(tileCoord) {
-					const z = tileCoord[0];
-					const x = tileCoord[1];
-					const y = -tileCoord[2] - 1;
-					return `https://tile${1 + (x + y) % 4}.openfreemap.org/tiles/v1/${z}/${x}/${y}.pbf`;
-				},
+				projection: 'EPSG:3857',
 				format: new ol.format.MVT(),
 				tileGrid: ol.tilegrid.createXYZ({
-					extent: ol.proj.get('EPSG:3857').getExtent(),
 					minZoom: 0,
 					maxZoom: 14,
 					tileSize: 512
 				}),
+				tileLoadFunction: function(tile, url) {
+					tile.setLoader(function(extent, resolution, projection) {
+						const tileCoord = this.getTileCoordForExtentAndResolution(
+							extent,
+							resolution,
+							projection
+						);
+						const z = tileCoord[0];
+						const x = tileCoord[1];
+						const y = -tileCoord[2] - 1;
+						const tileUrl = `https://tile${1 + (x + y) % 4}.openfreemap.org/tiles/v1/${z}/${x}/${y}.pbf`;
+						
+						fetch(tileUrl)
+							.then(response => response.arrayBuffer())
+							.then(buffer => {
+								const format = tile.getFormat();
+								const features = format.readFeatures(new Uint8Array(buffer), {
+									extent: extent,
+									featureProjection: projection
+								});
+								tile.setFeatures(features);
+								tile.setProjection(projection);
+								tile.setExtent(extent);
+							})
+							.catch(error => {
+								console.error('Error loading tile:', error);
+								tile.setFeatures([]);
+							});
+					}.bind(this));
+				},
 				attributions: [
 					'<a href="https://www.openstreetmap.org/copyright" target="_blank">OpenStreetMap contributors</a>',
 					'<a href="https://openfreemap.org/" target="_blank">OpenFreeMap</a>'
